7c124b7cffc095147bc45b6149e3b837a66a2284
Ahmed Khalil <109757454+DevAhmedKhalil@users.noreply.github.com>
2023-11-17 23:11:05 +0200

    Add files via upload
    Solutions of week 1 assignments.

---
+++ week1/week1.py
@@ -0,0 +1,17 @@
+# Assignment One In Week One
+print("Assignment 1 Week one")
+
+name, age, country = "Ahmed", "20", "EGY"
+print("Name: " + name)
+print("Age: " + age)
+print("Country: " + country)
+# print("Hello, My Name Is " + name + "And Iam " + age + " Years Old and I Live in " + country + ".")
+print("hello, my name is  " + name + "my age is " + age + "country is " + country)
+print(type(name), "\n", type(age), "\n", type(country), "\n")
+
+# create a variable password
+password = "123456"
+print("password: " + password)
+
+fullName = "ahmed"
+print("fullName " + fullName)
